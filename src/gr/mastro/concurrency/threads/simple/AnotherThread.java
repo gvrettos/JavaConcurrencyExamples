b56abf2db8915f53828d886436491c282a745709
George Vrettos <lukedevgr@gmail.com>
2018-08-09 01:10:58 +0300

    Play with Runnable, Thread, interrupt and join

---
+++ src/gr/mastro/concurrency/threads/simple/AnotherThread.java
@@ -0,0 +1,20 @@
+package gr.mastro.concurrency.threads.simple;
+
+public class AnotherThread extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("Hello from " + currentThread().getName());
+		
+		try {
+			Thread.sleep(3000);
+		}
+		catch (InterruptedException e) {
+			System.out.println("Another thread wake me up");
+			return;
+		}
+		
+		System.out.println("Three seconds have passed and I'm awake");
+	}
+
+}
