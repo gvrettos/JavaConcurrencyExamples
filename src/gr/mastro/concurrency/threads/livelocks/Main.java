4f3a8dbe0009b159889bd2bdefd70f5432f4174f
George Vrettos <lukedevgr@gmail.com>
2018-09-08 16:53:47 +0300

    Demonstrate multi-threaded application causing a live lock

---
+++ src/gr/mastro/concurrency/threads/livelocks/Main.java
@@ -0,0 +1,15 @@
+package gr.mastro.concurrency.threads.livelocks;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final Worker worker1 = new Worker("Worker 1", true);
+		final Worker worker2 = new Worker("Worker 2", true);
+		
+		final SharedResource sharedResource = new SharedResource(worker1);
+		
+		new Thread(() -> worker1.work(sharedResource, worker2)).start();
+		new Thread(() -> worker2.work(sharedResource, worker1)).start();
+	}
+
+}
